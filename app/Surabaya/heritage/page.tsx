5dc1bfdf826e9be5cf7e7b34fd7b90f00ffc4827
Bima Bimo <satriaabimanyu595@gmail.com>
2024-09-30 09:20:47 +0700

    refactor: refactor heritage now can be access for all user and it fetch properly

--- app/Surabaya/heritage/page.tsx
+++ app/Surabaya/heritage/page.tsx
@@ -1,14 +1,13 @@
-"use client";
-
+'use client';
 import React, { useState, useEffect } from 'react';
-import { Container, Card, CardContent, Typography, Box, Grid, Button } from '@mui/material';
+import { Grid, Button, Box, Container } from '@mui/material';
 
 interface TouristDestinationFile {
   link: string;
 }
 
 interface Destination {
-  id: number;
+  id: string;
   nameIndonesia: string;
   address: string;
   descriptionIndonesia: string;
@@ -19,133 +18,175 @@ interface Destination {
 
 export default function HeritagePage() {
   const [destinations, setDestinations] = useState<Destination[]>([]);
-  const [currentIndex, setCurrentIndex] = useState(0); 
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     const fetchData = async () => {
       const allDestinations: Destination[] = [];
-
-      // Loop through pages 1 to 7
       for (let page = 1; page <= 7; page++) {
-        const res = await fetch(`https://tourism.surabaya.go.id/api/kominfo/destination?page=${page}`);
+        const res = await fetch(
+          `https://tourism.surabaya.go.id/api/kominfo/destination?page=${page}`
+        );
         const result = await res.json();
-        allDestinations.push(...result.data?.data || []);
+        allDestinations.push(...(result.data?.data || []));
       }
-
       setDestinations(allDestinations);
     };
     fetchData();
   }, []);
 
+  const excludedIds = [
+    '9bf9c9fe-51a6-4b0b-97aa-9c8ce15074a5',
+    '9b2e8c13-499b-47bd-9b47-827d0afd00e9',
+    '9bfbf031-1016-4e9e-949e-aa387960d040',
+    '9bfbfbeb-db56-4ac7-81d2-ea635c013c47',
+    '9bfbfcee-56b4-4fcb-84d0-87c6509d06ff',
+    '9bfbff40-58ee-4f11-bb8c-0bbd890a4d0d',
+    '9bfc06ef-5862-42aa-ac91-d2679e7f4073',
+    '9bfc084c-a856-4a26-9af3-f8cc3b461117',
+    '9bfc08b2-5828-44de-b06d-558c57a35bbb',
+    '9bfc0914-44c0-4e21-bcfc-ab3d57178efc',
+    '9bfc0aa9-82c8-430a-9777-59ee0a3112f0',
+    "9bfc07a1-9d7b-4f92-89bc-7cdfb7eb2825",
+  ];
+
   const filteredDestinations = destinations.filter(
-    (destination) => destination.touristDestinationFiles.length > 0
+    (destination) =>
+      destination.touristDestinationFiles.length > 0 &&
+      !excludedIds.includes(destination.id)
   );
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) =>
-        prevIndex === filteredDestinations.length - 1 ? 0 : prevIndex + 1
-      );
-    }, 3000); 
+  const truncateDescription = (description: string) => {
+    return description.length > 222
+      ? description.slice(0, 222) + '...'
+      : description;
+  };
 
-    return () => clearInterval(interval);
-  }, [filteredDestinations.length]);
-
-  if (filteredDestinations.length === 0) {
-    return <div>No destinations available.</div>;
-  }
+  const openLocationInMaps = (latitude: number, longitude: number) => {
+    const url = `https://www.google.com/maps?q=${latitude},${longitude}`;
+    window.open(url, '_blank');
+  };
 
   return (
-    <Container>
-      <Box mb={4} display="flex" alignItems="center" justifyContent="center" position="relative">
+    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+      <Box
+        mb={4}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        position="relative"
+      >
         {filteredDestinations.length > 0 && (
           <>
-            {/* Current Image */}
             <img
-              src={filteredDestinations[currentIndex].touristDestinationFiles[0].link}
+              src={
+                filteredDestinations[currentIndex].touristDestinationFiles[0]
+                  .link
+              }
               alt={filteredDestinations[currentIndex].nameIndonesia}
-              style={{ width: '100%', height: '500px', objectFit: 'cover' }}
+              style={{
+                width: '100%',
+                height: '500px',
+                objectFit: 'cover',
+                borderRadius: '8px',
+              }}
             />
             <Box
               display="flex"
               justifyContent="space-between"
               position="absolute"
-              left="20px" 
+              left="20px"
               bottom="20px"
-              width="auto" 
+              width="auto"
             >
               <Button
-                onClick={() => setCurrentIndex((prevIndex) => prevIndex === 0 ? filteredDestinations.length - 1 : prevIndex - 1)}
-                variant="outlined" 
+                onClick={() =>
+                  setCurrentIndex((prevIndex) =>
+                    prevIndex === 0
+                      ? filteredDestinations.length - 1
+                      : prevIndex - 1
+                  )
+                }
+                variant="outlined"
                 style={{
-                  borderRadius: '50%', 
-                  minWidth: '40px', 
+                  borderRadius: '50%',
+                  minWidth: '40px',
                   minHeight: '40px',
                   display: 'flex',
                   alignItems: 'center',
                   justifyContent: 'center',
-                  padding: '0', 
-                  borderColor: 'white', 
-                  color: 'white', 
+                  padding: '0',
+                  borderColor: 'white',
+                  color: 'white',
                 }}
               >
-                <span style={{ fontSize: '20px', lineHeight: '0' }}>&lt;</span> {/* Left arrow */}
+                <span style={{ fontSize: '20px', lineHeight: '0' }}>&lt;</span>
               </Button>
               <Button
-                onClick={() => setCurrentIndex((prevIndex) => prevIndex === filteredDestinations.length - 1 ? 0 : prevIndex + 1)}
+                onClick={() =>
+                  setCurrentIndex((prevIndex) =>
+                    prevIndex === filteredDestinations.length - 1
+                      ? 0
+                      : prevIndex + 1
+                  )
+                }
                 variant="outlined"
                 style={{
-                  borderRadius: '50%', 
-                  minWidth: '40px', 
-                  minHeight: '40px', 
+                  borderRadius: '50%',
+                  minWidth: '40px',
+                  minHeight: '40px',
                   display: 'flex',
                   alignItems: 'center',
                   justifyContent: 'center',
-                  padding: '0', 
+                  padding: '0',
                   borderColor: 'white',
-                  color: 'white', 
-                  marginLeft: '10px' 
+                  color: 'white',
+                  marginLeft: '10px',
                 }}
               >
-                <span style={{ fontSize: '20px', lineHeight: '0' }}>&gt;</span> {/* Right arrow */}
+                <span style={{ fontSize: '20px', lineHeight: '0' }}>&gt;</span>
               </Button>
             </Box>
           </>
         )}
       </Box>
 
-      <Typography variant="h4" component="h1" gutterBottom>
-        Tourist Destinations
-      </Typography>
       <Grid container spacing={4}>
-        {destinations.map((destination) => (
+        {filteredDestinations.map((destination) => (
           <Grid item xs={12} sm={6} md={4} key={destination.id}>
-            <Card className="border p-4">
+            <div className="bg-[#add8e6] rounded-lg shadow-md p-4 flex flex-col justify-between h-full">
               {destination.touristDestinationFiles.length > 0 && (
                 <img
                   src={destination.touristDestinationFiles[0].link}
                   alt={destination.nameIndonesia}
-                  style={{ width: '100%', height: '200px', objectFit: 'cover' }}
+                  className="w-full h-56 object-cover rounded-t-lg"
+                  style={{ maxHeight: '200px', objectFit: 'cover' }}
                 />
               )}
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
+              <div
+                className="p-4"
+                style={{ minHeight: '300px', position: 'relative' }}
+              >
+                <h3 className="text-lg font-bold text-black mb-2">
                   {destination.nameIndonesia}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  <strong>Address:</strong> {destination.address}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {destination.descriptionIndonesia}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  <strong>Latitude:</strong> {destination.latitude}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  <strong>Longitude:</strong> {destination.longitude}
-                </Typography>
-              </CardContent>
-            </Card>
+                </h3>
+                <p className="text-sm text-gray-500 mb-4">
+                  {truncateDescription(destination.descriptionIndonesia)}
+                </p>
+                <button
+                  onClick={() =>
+                    openLocationInMaps(
+                      destination.latitude,
+                      destination.longitude
+                    )
+                  }
+                  className="bg-[#2b8ea6] text-white py-2 px-4 rounded-full hover:bg-[#257a8a] transition duration-300"
+                  style={{ position: 'absolute', bottom: '16px', left: '16px' }}
+                >
+                  Menuju Lokasi
+                </button>
+              </div>
+            </div>
           </Grid>
         ))}
       </Grid>
