94a3c333f535e007f9909c2ebbbc533ab0238322
Bima Bimo <satriaabimanyu595@gmail.com>
2024-10-07 15:53:02 +0700

    feat: Register Page now was useable from backend

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,102 +2,129 @@
 
 import React, { useState } from 'react';
 import { Button, Box, Typography, Grid } from '@mui/material';
-
+import { City } from '../utils/listKota';
+import Swal from 'sweetalert2';
+import { useRouter } from 'next/navigation';
 interface FormData {
-  asalKota: string;
-  nama: string;
-  namaAjudan: string;
-  nomorHandphone: string;
-  tanggalDatang: string;
-  waktuDatang: string;
+  asal_kota: string;
+  nama_walikota: string;
+  nama_ajudan: string;
+  nomor_handphone: string;
+  tanggal_kedatangan: string;
+  jam_kedatangan: string;
   pesawat: string;
-  tanggalPulang: string;
-  lokasiMenginap: string;
-  jumlahRombongan: string;
+  tanggal_kepulangan: string;
+  lokasi_menginap: string;
+  jumlah_rombongan: string;
 }
 
 export default function Register() {
   const [formData, setFormData] = useState<FormData>({
-    asalKota: '',
-    nama: '',
-    namaAjudan: '',
-    nomorHandphone: '',
-    tanggalDatang: '',
-    waktuDatang: '',
+    asal_kota: '',
+    nama_walikota: '',
+    nama_ajudan: '',
+    nomor_handphone: '',
+    tanggal_kedatangan: '',
+    jam_kedatangan: '',
     pesawat: '',
-    tanggalPulang: '',
-    lokasiMenginap: '',
-    jumlahRombongan: '',
+    tanggal_kepulangan: '',
+    lokasi_menginap: '',
+    jumlah_rombongan: '',
   });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+  const router = useRouter();
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLSelectElement | HTMLInputElement>
+  ): void => {
     const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
+
+    if (name === 'asal_kota') {
+      setFormData({
+        ...formData,
+        [name]: value as City,
+      });
+    } else {
+      setFormData({
+        ...formData,
+        [name]: value,
+      });
+    }
   };
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
+  const handleSubmit = async (
+    e: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
     e.preventDefault();
-  
+
+    const tanggalKedatangan = new Date(
+      `${formData.tanggal_kedatangan}T${formData.jam_kedatangan}:00`
+    ).toISOString();
+    const tanggalKepulangan = new Date(
+      `${formData.tanggal_kepulangan}T00:00:00`
+    ).toISOString();
+
     const payload = {
-      data: {
-        asal_kota: formData.asalKota,
-        Nama: formData.nama,
-        nama_ajudan: formData.namaAjudan,
-        nomor_handphone: formData.nomorHandphone,
-        tanggal_datang: formData.tanggalDatang,
-        waktu_datang: `${formData.waktuDatang}:00`,  // Ensure seconds are added
-        pesawat: formData.pesawat,
-        tanggal_pulang: formData.tanggalPulang,
-        lokasi_menginap: formData.lokasiMenginap,
-        jumlah_rombongan: Number(formData.jumlahRombongan),
-      },
+      asal_kota: formData.asal_kota,
+      nama_walikota: formData.nama_walikota,
+      nama_ajudan: formData.nama_ajudan,
+      nomor_handphone: formData.nomor_handphone,
+      jam_kedatangan: formData.jam_kedatangan,
+      pesawat: formData.pesawat,
+      tanggal_kepulangan: tanggalKepulangan,
+      lokasi_menginap: formData.lokasi_menginap,
+      jumlah_rombongan: Number(formData.jumlah_rombongan),
+      tanggal_kedatangan: tanggalKedatangan,
     };
-  
-    console.log("Submitting payload:", payload);
-  
+
     try {
-      const response = await fetch('http://localhost:1337/api/pendaftars', {
+      const response = await fetch('http://localhost:5000/peserta', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(payload),
       });
-  
+
       if (!response.ok) {
         const errorData = await response.json();
-        console.log("Error response:", errorData);
+        console.log('Error response:', errorData);
         throw new Error('Failed to submit data');
       }
-  
+
       const result = await response.json();
       console.log('Form Submitted Successfully:', result);
-  
-      // Reset form if needed
+
       setFormData({
-        asalKota: '',
-        nama: '',
-        namaAjudan: '',
-        nomorHandphone: '',
-        tanggalDatang: '',
-        waktuDatang: '',
+        asal_kota: '',
+        nama_walikota: '',
+        nama_ajudan: '',
+        nomor_handphone: '',
+        tanggal_kedatangan: '',
+        jam_kedatangan: '',
         pesawat: '',
-        tanggalPulang: '',
-        lokasiMenginap: '',
-        jumlahRombongan: '',
+        tanggal_kepulangan: '',
+        lokasi_menginap: '',
+        jumlah_rombongan: '',
       });
-  
-      alert('Pendaftaran berhasil!');
-  
+
+      Swal.fire({
+        title: "Selamat Datang Rek!",
+        text: "Anda Terdaftar di Munas 2025!",
+        icon: "success"
+      }).then(() => {
+        router.push('/'); 
+      });
     } catch (error) {
       console.error('Error submitting form:', error);
-      alert('Pendaftaran gagal. Silakan coba lagi.');
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
+      });
     }
   };
-  
+
   return (
     <Grid container sx={{ minHeight: '100vh' }}>
       <Grid
@@ -131,7 +158,7 @@ export default function Register() {
         alignItems="center"
         justifyContent="center"
       >
-        <Box
+        {/* <Box
           component="img"
           src="/sby.png"
           alt="Logo"
@@ -140,11 +167,11 @@ export default function Register() {
             height: 'auto',
             mb: 3,
           }}
-        />
+        /> */}
 
         <Box
           sx={{
-            width: { xs: '100%', md: 600 }, // Responsive width
+            width: { xs: '100%', md: 600 },
             p: 4,
             bgcolor: 'white',
             borderRadius: 2,
@@ -152,134 +179,95 @@ export default function Register() {
             textAlign: 'center',
           }}
         >
-          <Typography variant="h5" align="center" gutterBottom>
+          <Typography variant="h5" align="left" gutterBottom className='font-bold text-body mb-11'>
             Register
-          </Typography>
+          </Typography> 
 
           <form onSubmit={handleSubmit}>
-            <Grid container spacing={2}>
-              {/* Row 1 */}
+            <Grid container spacing={2} className='text-body'>
               <Grid item xs={12} md={6}>
-                <Typography>Asal Kota</Typography>
-                <input
-                  type="text"
-                  name="asalKota"
-                  value={formData.asalKota}
+                <Typography align="left">Asal Kota</Typography>
+                <select
+                  name="asal_kota"
+                  value={formData.asal_kota}
                   onChange={handleChange}
                   required
-                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-secondary"
-                />
-              </Grid>
-              <Grid item xs={12} md={6}>
-                <Typography>Nama</Typography>
-                <input
-                  type="text"
-                  name="nama"
-                  value={formData.nama}
-                  onChange={handleChange}
-                  required
-                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-secondary"
-                />
+                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 transition-all"
+                >
+                  <option value="" disabled>
+                    Select Kota
+                  </option>
+                  {Object.values(City).map((kota) => (
+                    <option key={kota} value={kota}>
+                      {kota.replace(/_/g, ' ')}
+                    </option>
+                  ))}
+                </select>
               </Grid>
 
-              {/* Row 2 */}
-              <Grid item xs={12} md={6}>
-                <Typography>Nama Ajudan</Typography>
-                <input
-                  type="text"
-                  name="namaAjudan"
-                  value={formData.namaAjudan}
-                  onChange={handleChange}
-                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-secondary"
-                />
-              </Grid>
-              <Grid item xs={12} md={6}>
-                <Typography>Nomor Handphone</Typography>
-                <input
-                  type="tel"
-                  name="nomorHandphone"
-                  value={formData.nomorHandphone}
-                  onChange={handleChange}
-                  required
-                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-secondary"
-                />
-              </Grid>
-
-              {/* Row 3 */}
-              <Grid item xs={12} md={6}>
-                <Typography>Tanggal Datang</Typography>
-                <input
-                  type="date"
-                  name="tanggalDatang"
-                  value={formData.tanggalDatang}
-                  onChange={handleChange}
-                  required
-                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-secondary"
-                />
-              </Grid>
-              <Grid item xs={12} md={6}>
-                <Typography>Waktu Datang</Typography>
-                <input
-                  type="time"
-                  name="waktuDatang"
-                  value={formData.waktuDatang}
-                  onChange={handleChange}
-                  required
-                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-secondary"
-                />
-              </Grid>
-
-              {/* Row 4 */}
-              <Grid item xs={12} md={6}>
-                <Typography>Pesawat</Typography>
-                <input
-                  type="text"
-                  name="pesawat"
-                  value={formData.pesawat}
-                  onChange={handleChange}
-                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-secondary"
-                />
-              </Grid>
-              <Grid item xs={12} md={6}>
-                <Typography>Tanggal Pulang</Typography>
-                <input
-                  type="date"
-                  name="tanggalPulang"
-                  value={formData.tanggalPulang}
-                  onChange={handleChange}
-                  required
-                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-secondary"
-                />
-              </Grid>
-
-              {/* Row 5 */}
-              <Grid item xs={12} md={6}>
-                <Typography>Lokasi Menginap</Typography>
-                <input
-                  type="text"
-                  name="lokasiMenginap"
-                  value={formData.lokasiMenginap}
-                  onChange={handleChange}
-                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-secondary"
-                />
-              </Grid>
-              <Grid item xs={12} md={6}>
-                <Typography>Jumlah Rombongan</Typography>
-                <input
-                  type="number"
-                  name="jumlahRombongan"
-                  value={formData.jumlahRombongan}
-                  onChange={handleChange}
-                  required
-                  className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-secondary"
-                />
-              </Grid>
+              {[
+                {
+                  label: 'Nama Walikota',
+                  name: 'nama_walikota',
+                  type: 'text',
+                  required: true,
+                },
+                { label: 'Nama Ajudan', name: 'nama_ajudan', type: 'text' },
+                {
+                  label: 'Nomor Handphone',
+                  name: 'nomor_handphone',
+                  type: 'tel',
+                  required: true,
+                },
+                {
+                  label: 'Tanggal Datang',
+                  name: 'tanggal_kedatangan',
+                  type: 'date',
+                  required: true,
+                },
+                {
+                  label: 'Waktu Datang',
+                  name: 'jam_kedatangan',
+                  type: 'time',
+                  required: true,
+                },
+                { label: 'Pesawat', name: 'pesawat', type: 'text' },
+                {
+                  label: 'Tanggal Pulang',
+                  name: 'tanggal_kepulangan',
+                  type: 'date',
+                  required: true,
+                },
+                {
+                  label: 'Lokasi Menginap',
+                  name: 'lokasi_menginap',
+                  type: 'text',
+                },
+                {
+                  label: 'Jumlah Rombongan',
+                  name: 'jumlah_rombongan',
+                  type: 'number',
+                  required: true,
+                },
+              ].map((input) => (
+                <Grid item xs={12} md={6} key={input.name}>
+                  <Typography align="left">{input.label}</Typography>
+                  <input
+                    type={input.type}
+                    name={input.name}
+                    value={formData[input.name as keyof FormData]}
+                    onChange={handleChange}
+                    required={input.required}
+                    className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 transition-all"
+                  />
+                </Grid>
+              ))}
             </Grid>
 
             <Button
               type="submit"
               variant="contained"
-              color="primary"
+              className='bg-second'
               fullWidth
               sx={{ mt: 2 }}
             >
