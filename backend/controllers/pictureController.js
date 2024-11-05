27b173a931a5324d0073d0122b4c71744d10eb5f
Bima Bimo <91863257+Bima595@users.noreply.github.com>
2024-11-05 09:20:01 +0700

    Delete backend directory

--- backend/controllers/pictureController.js
+++
@@ -1,5 +0,0 @@
-export const getPicture = (req, res) => {};
-
-export const createPicture = (req, res) => {};
-
-export const deletePicture = (req, res) => {};
--- backend/prisma/migrations/20241003080215_rename_migration_variable/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `datamunas` MODIFY `jamKedatangan` VARCHAR(191) NOT NULL;
